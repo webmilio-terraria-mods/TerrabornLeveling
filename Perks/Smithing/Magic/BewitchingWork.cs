b9ac53d155e12e13e859cf11decc78996b554dd5
webmilio <webmilio@users.noreply.github.com>
2021-11-07 11:43:17 -0500

    Started Magic smithing branch.

---
+++ Perks/Smithing/Magic/BewitchingWork.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.ID;
+
+namespace TerrabornLeveling.Perks.Smithing.Magic;
+
+[Parents(typeof(BeginnersFlame))]
+public class BewitchingWork : MagicPerk
+{
+    public BewitchingWork() : base("bewitchingwork")
+    {
+    }
+
+    public override string Name => "Bewitching Work";
+
+    protected override int RequiredSkill => 5;
+
+    protected override int[] Unlocks { get; } = { PrefixID.Taboo, PrefixID.Manic };
+    protected override object[] UnlockNames { get; } = { nameof(PrefixID.Taboo), nameof(PrefixID.Manic) };
+
+    public override IPerkVisualDescriptor Visuals { get; } = new StandardPerkVisualDescriptor(new(XPosition, YPosition - YOffset * 0));
+}
---
+++ Perks/Smithing/Magic/MagicPerk.cs
@@ -0,0 +1,19 @@
+﻿namespace TerrabornLeveling.Perks.Smithing.Magic;
+
+public abstract class MagicPerk : ModifiersUnlockingPerk
+{
+    protected const float
+        XPosition = .6f,
+        YPosition = .9f;
+
+    private const string MagicFormat = "Unlocks the {0} and {1} modifiers for magic weapons.";
+
+    protected MagicPerk(string identifier) : base(identifier)
+    {
+    }
+
+    protected override string GetDescriptionString()
+    {
+        return MagicFormat;
+    }
+}
